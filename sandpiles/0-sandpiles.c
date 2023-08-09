ef843c64152952661386dfc8201ab4a6d699ef34
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2023-08-09 15:55:26 +0200

    sandpiles

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,17 +1,36 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "sandpiles.h"
 
 /**
- * sandpiles_sum - function that computes the sum of two sandpiles
- * @grid1: Left 3x3 grid
- * @grid2: Right 3x3 grid
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
  */
+static void print_grid(int grid[3][3])
+{
+    int i, j;
 
+    printf("=\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j != 0)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/**
+ * sandpiles_sum - Computes the sum of two sandpiles
+ * @grid1: First sandpile grid
+ * @grid2: Second sandpile grid
+ */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i = 0;
-    int j = 0;
+    int i, j, unstable;
+    int grid3[3][3];
 
     for (i = 0; i < 3; i++)
     {
@@ -20,4 +39,44 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
             grid1[i][j] += grid2[i][j];
         }
     }
+
+    do {
+        unstable = 0;
+
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 3; j++)
+            {
+                if (grid1[i][j] > 3)
+                {
+                    unstable = 1;
+                    grid1[i][j] -= 4;
+
+                    if (i - 1 >= 0)
+                        grid3[i - 1][j] += 1;
+                    if (i + 1 < 3)
+                        grid3[i + 1][j] += 1;
+                    if (j - 1 >= 0)
+                        grid3[i][j - 1] += 1;
+                    if (j + 1 < 3)
+                        grid3[i][j + 1] += 1;
+                }
+            }
+        }
+
+        if (unstable)
+        {
+            printf("=\n");
+            print_grid(grid1);
+
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    grid1[i][j] += grid3[i][j];
+                    grid3[i][j] = 0;
+                }
+            }
+        }
+    } while (unstable);
 }
