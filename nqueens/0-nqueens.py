882e1cd1a3d6912d222330785568f727a385425b
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2023-09-21 09:56:33 +0200

    nqueens

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,58 +1,73 @@
 #!/usr/bin/python3
-"""queens puzzle is the challenge of placing N
-non-attacking queens on an N×N chessboard. Write
-a program that solves the N queens problem"""
+"""
+The N queens puzzle
+"""
 
 
 import sys
 
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+try:
+    int(sys.argv[1])
+except Exception:
+    print("N must be a number")
+    exit(1)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+if not isinstance(int(sys.argv[1]), int):
+    print("N must be a number")
+    exit(1)
 
-def nqueens(n: int):
-    """queens puzzle is the challenge of placing N
-non-attacking queens on an N×N chessboard. Write
-a program that solves the N queens problem"""
-    columns = [0] * n
-
-    def valid(row, col):
-        """chack if the position is valid to place a nqueens"""
-        for i in range(row):
-            if columns[i] == col:
-                return False
-            if columns[i] - i == col - row:
-                return False
-            if columns[i] + i == col + row:
-                return False
-        return True
-
-    result = []
-
-    def backtrack(row):
-        """function to place nqueens"""
-        if row != n:
-            for col in range(n):
-                if valid(row, col):
-                    columns[row] = col
-                    backtrack(row + 1)
-        else:
-            result.append([[i, columns[i]] for i in range(n)])
-
-    backtrack(0)
-    for board in result:
-        print(board)
+n = int(sys.argv[1])
 
 
-if __name__ == "__main__":
-    """Usage: nqueens N
-    where N must be an integer greater or equal to 4
-    The program should print every possible solution
-    to the problem"""
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    if not sys.argv[1].isdigit():
-        print("N must be a number")
-        exit(1)
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        exit(1)
-    nqueens(int(sys.argv[1]))
+def is_valid_state(state, n):
+    return len(state) == n
+
+
+def get_candidates(state, n):
+    if not state:
+        return range(n)
+
+    position = len(state)
+    candidates = set(range(n))
+    for row, col in enumerate(state):
+        candidates.discard(col)
+        dist = position - row
+        candidates.discard(col + dist)
+        candidates.discard(col - dist)
+    return candidates
+
+
+def search(state, solutions, n):
+    if is_valid_state(state, n):
+        state_string = state_to_string(state)
+        solutions.append(state_string)
+        return
+
+    for candidate in get_candidates(state, n):
+        state.append(candidate)
+        search(state, solutions, n)
+        state.pop()
+
+
+def solveNQueens(n):
+    solutions = []
+    state = []
+    search(state, solutions, n)
+    return solutions
+
+
+def state_to_string(state):
+    res = []
+
+    for x, y in enumerate(state):
+        res.append([x, y])
+    return res
+
+
+for solution in solveNQueens(n):
+    print(solution)
