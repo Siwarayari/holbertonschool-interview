9596b27791f1c8de09fdd21673ab06bc15503d08
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2023-11-22 14:59:03 +0100

    HEADER FILE

---
+++ infinite_multiplication/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void _puts(char *str);
+
+#endif /* HOLBERTON_H */
