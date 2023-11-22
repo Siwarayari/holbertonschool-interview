651ca8f804f7c0f627c12d6d8fc9db2ec52af690
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2023-11-22 20:17:19 +0100

    HEADER FILE

---
+++ merge_sort/sort.h
@@ -0,0 +1,8 @@
+#ifndef SORT_H
+#define SORT_H
+
+void print_array(const int *array, size_t size);
+void merge(int *arr, int *left, int *right, size_t size);
+void merge_sort(int *array, size_t size);
+
+#endif /* SORT_H */
