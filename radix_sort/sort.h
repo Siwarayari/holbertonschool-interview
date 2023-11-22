bf757f9fa07816b013e2bc05667fc7ff6129ae6f
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2023-11-22 21:02:33 +0100

    HEADER FILE

---
+++ radix_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_array(const int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void count_Sort(int *array, int size, int pos);
+
+#endif /* LISTS_H */
